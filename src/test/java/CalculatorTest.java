01757604971c91d7b56a434a4e9233bcb130e2c3
akshaytiwari29 <unpredictablerr@gmail.com>
2019-01-11 15:19:56 +0000

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,8 @@
+import org.junit.Test;
 import junit.framework.TestCase;
 public class CalculatorTest extends TestCase {
 
+  @Test 
   public void testAddition() {
     Calculator calc = new Calculator();
     // 3 + 4 = 7
@@ -8,7 +10,7 @@ public class CalculatorTest extends TestCase {
     int actual = calc.add(3, 4);
     assertEquals("adding 3 and 4", expected, actual);
   }
-
+  @Test
   public void testDivision() {
     Calculator calc = new Calculator();
     int expected = 2;
