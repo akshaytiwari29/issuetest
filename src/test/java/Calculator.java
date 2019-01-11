4d2d786f77345f2861f91b827fdbfabca6e8c8ef
akshaytiwari29 <unpredictablerr@gmail.com>
2019-01-11 20:54:53 +0000

    Update Calculator.java

--- src/test/java/Calculator.java
+++ src/test/java/Calculator.java
@@ -1,4 +1,3 @@
-
 public class Calculator 
 {
 	public int add(int a,int b)
@@ -12,6 +11,15 @@ public class Calculator
 	public int substract(int a,int b)
 	{
 		int c=a-b;
+		return  c;
+		
+		
+	}
+	
+	
+	public int mul(int a,int b)
+	{
+		int c=a*b;
 		return  c;
 		
 		
