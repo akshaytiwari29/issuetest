eba8b25704eee9f0ff190c99e69cd57f56971957
akshaytiwari29 <unpredictablerr@gmail.com>
2019-01-11 21:58:17 +0000

    Update LoginApplicationTests.java

--- src/test/java/LoginApplicationTests.java
+++ src/test/java/LoginApplicationTests.java
@@ -94,6 +94,25 @@ public class LoginApplicationTests {
                     // TODO Auto-generated method stub
                     
     }
+    @Test
+	public void whenEmptyString_thenAccept() {
+	    Pallindrome palindromeTester = new Pallindrome();
+	    assertTrue(palindromeTester.isPalindrome(""));
+	}
+	
+	
+	@Test
+	public void whenPalindrom_thenAccept() {
+	    Pallindrome palindromeTester = new Pallindrome();
+	    assertTrue(palindromeTester.isPalindrome("noon"));
+	}
+	     
+	@Test
+	public void whenNearPalindrom_thanReject(){
+	    Pallindrome palindromeTester = new Pallindrome();
+	    assertFalse(palindromeTester.isPalindrome("neon"));
+	}
+
 
 
 }
